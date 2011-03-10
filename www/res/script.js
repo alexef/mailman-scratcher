294966c70e87e25ac1407dbee45baaaf204ff902
Alex Eftimie <alex@eftimie.ro>
2011-03-10 14:11:10 +0200

    fixed wrong title

--- www/res/script.js
+++ www/res/script.js
@@ -1,15 +1,17 @@
 /* Move first word from h2, i.e. cut [Doctorat-anunturi] */
 
 $(document).ready(function () {
-  text = $('h2').html();
-  parts = text.split(']');
-  while (parts[0].trim()[0] == '[') {
-    label = parts.shift().trim().slice(1);
-    $('#list-label').prepend('<span>&bull; ' + label + '</span>');
-  }
-  
-  text = parts.join(' ');
-  $('h2').html(text);
+  $('h2').each(function() {
+		  text = $(this).html();
+		  parts = text.split(']');
+		  while (parts[0].trim()[0] == '[') {
+			label = parts.shift().trim().slice(1);
+			$('#list-label').prepend('<span>&bull; ' + label + '</span>');
+		  }
+		  
+		  text = parts.join(' ');
+		  $(this).html(text);
+  });
 });
   
   
